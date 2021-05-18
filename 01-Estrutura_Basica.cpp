60dc6b831c66dca255a4cce56449a92089f833b6
Alexandre Neves <alexandreneves98@outlook.com>
2021-05-18 13:19:19 -0300

    01-Estrutura Basica da Linguagem C++

---
+++ 01-Estrutura_Basica.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // <- Biblioteca de entra e saida 
+
+using namespace std; // Este comando serve para definir um "espaço de nomes", ou namespace.
+
+// Função Main principal do programa. 
+int main(int argc, char const *argv[])
+{
+    system("cls"); // <- Limpar o termina.
+
+    cout<< "Ola mundo"; // É um objeto de saida, então imprimira conteudo no termina. 
+
+    // <- Para indicarmos um comentário em c++.
+
+    return 0; //<- Em uma função que não seja void, sempre devemos retornar alguma coisa. 
+}
